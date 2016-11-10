c335c9f8f04b63f65cae86b4b0a14e840f9a1fa4
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-10 01:04:49 -0500

    Move url variables and clean url func to own module

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import axios from 'axios'
 import './App.css'
+import { clearUrlParams, SERVER_URL, REDIRECT_URI } from './utils'
 import User from './User'
-
-const clearUrlParams = () => {
-  const query = window.location.search.substring(1)
-  if (query.length) {
-    if (window.history !== undefined && window.history.pushState !== undefined) {
-      window.history.pushState({}, document.title, window.location.pathname)
-    }
-  }
-}
 
 class App extends Component {
   constructor () {
@@ -28,12 +20,9 @@ class App extends Component {
     const url = window.location.search
     const parsedUrl = new URLSearchParams(url)
     if (parsedUrl.has('code')) {
-      const ROOT_URL = process.env.NODE_ENV === 'development'
-        ? 'http://localhost:4567/instagram'
-        : 'https://sinatra-oauth-backend.herokuapp.com/instagram'
       const code = parsedUrl.get('code')
 
-      axios.get(`${ROOT_URL}?code=${code}`)
+      axios.get(`${SERVER_URL}?code=${code}`)
       .then(res => {
         if (res.data.code === 400) {
           console.log('Instagram api call error', res.data)
@@ -69,14 +58,11 @@ class App extends Component {
       authenticated,
       response
     } = this.state
-    const redirect_uri = process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3001'
-      : 'https://gp3gp3gp3.github.io/instagram-clone'
 
     if (!authenticated) {
       return (
         <a
-          href={`https://api.instagram.com/oauth/authorize/?client_id=f98db0ad5d2648f095525ea0986f4d1a&redirect_uri=${redirect_uri}&response_type=code`}
+          href={`https://api.instagram.com/oauth/authorize/?client_id=f98db0ad5d2648f095525ea0986f4d1a&redirect_uri=${REDIRECT_URI}&response_type=code`}
         >Login to Instagram</a>
       )
     } else {
