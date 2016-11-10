4a8ce59b1b8a3934928aa1fc9c7d27a80fc3690a
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-10 00:28:18 -0500

    Add state to app, set localStorage for token

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      authenticated: false,
+      response: {}
+    }
+    this.renderUser = this.renderUser.bind(this)
+  }
+
   componentWillMount () {
     const url = window.location.search
     const parsedUrl = new URLSearchParams(url)
     if (parsedUrl.has('code')) {
       const ROOT_URL = process.env.NODE_ENV === 'development'
-      ? 'http://localhost:4567/instagram'
-      : 'https://sinatra-oauth-backend.herokuapp.com/instagram'
+        ? 'http://localhost:4567/instagram'
+        : 'https://sinatra-oauth-backend.herokuapp.com/instagram'
       const code = parsedUrl.get('code')
 
       axios.get(`${ROOT_URL}?code=${code}`)
@@ -18,17 +27,35 @@ class App extends Component {
         if (res.data.code === 400) {
           this.setState({error: res.data})
         } else {
-          // set local storage off res.data.authentication_token
-          this.setState({response: res.data.user})
+          localStorage.setItem('token', res.data.access_token)
+          this.setState({
+            response: res.data.user,
+            authenticated: true
+          })
 
         }
       })
       .catch(error => {
-        console.log("sad", error)
+        console.log("Sinatra server error", error)
         this.setState({error})
       })
     }
+  }
 
+  renderUser () {
+    /* Set localhost:3001 to https://gp3gp3gp3.github.io/instagram-clone */
+    const {
+      authenticated,
+      response
+    } = this.state
+    if (!authenticated) {
+      return <a href='https://api.instagram.com/oauth/authorize/?client_id=f98db0ad5d2648f095525ea0986f4d1a&redirect_uri=http://localhost:3001&response_type=code'>Login to Instagram</a>
+    } else {
+      console.log(response)
+      return (
+        <div>content</div>
+      )
+    }
   }
 
   render() {
@@ -38,9 +65,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Instagram clone</h2>
         </div>
-        <p className="App-intro">
-          <a href='https://api.instagram.com/oauth/authorize/?client_id=f98db0ad5d2648f095525ea0986f4d1a&redirect_uri=http://localhost:3001&response_type=code'>Login to instagram</a>
-        </p>
+        <div className="App-intro">
+          {this.renderUser()}
+        </div>
       </div>
     );
   }
